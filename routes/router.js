6331733b131f4882f7a5da7b7cffc7df0a3ffaf7
KansalAshutosh <kansalashutosh24@gmail.com>
2023-06-18 04:41:42 -0400

    Added Contact List, Add User and Update User

---
+++ routes/router.js
@@ -0,0 +1,37 @@
+const express = require("express");
+var router = express.Router();
+
+const services = require("../server/services/render");
+const controller = require("../server/controller/controller");
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Ashutosh's Portfolio" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact Me" });
+});
+
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+
+router.get("/projects", function (req, res, next) {
+  res.render("projects", { title: "Projects" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About Me" });
+});
+
+router.get("/listofcontacts", services.homeRoutes);
+router.get("/add-user", services.add_user);
+router.get("/update-user", services.update_user);
+
+//API
+router.post("/api/users", controller.create);
+router.get("/api/users", controller.find);
+router.put("/api/users/:id", controller.update);
+router.delete("/api/users/:id", controller.delete);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+const connectDB = require("./server/database/connection");
+
+//mongoDB connection
+connectDB();
+
 const app = express();
-const port = 3000;
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+
+//logging http request into console
+app.use(morgan("tiny"));
+
+//pass request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
 
 // view engine setup
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+//loading assets
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-let indexRouter = require("./routes/index");
+//importing routes
+let indexRouter = require("./routes/router");
 app.use("/", indexRouter);
 
-app.listen(port, () => {
-  console.log(`Running`);
+//port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
