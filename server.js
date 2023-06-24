935dcf0f410ada4f1b9720a38956d9fba5448448
KansalAshutosh <101134947+KansalAshutosh@users.noreply.github.com>
2023-06-24 23:38:48 +0000

    Changed the default localhost port

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get("/logout", (req, res) => {
 
 // Load environment variables
 dotenv.config({ path: "config.env" });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 //logging http request into console
 app.use(morgan("tiny"));
